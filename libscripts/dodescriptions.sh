d806df266dfbef619afa9f8a2a988801951161b0
Paul  Cav <pacav69@gmail.com>
2018-04-17 19:48:00 +1000

    updated DESCRIPTION
    added Firebeetle ESP32

--- libscripts/dodescriptions.sh
+++ libscripts/dodescriptions.sh
@@ -1,4 +1,6 @@
 ## this will print the current folder and create a DESCRIPTION file
 ## and append files list recursively
 ## for use in Eagle cad Control panel 
+# to run goto external_lbrs directory and type 
+# ./libscripts/dodescriptions.sh
 result=${PWD##*/} | printf '%s\n' "${PWD##*/}" >DESCRIPTION | ls -R >>DESCRIPTION 
